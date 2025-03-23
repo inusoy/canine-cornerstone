e4a01eb7e2b2d64be0a40ad57d5500fba940ee67
Martin.Machnikowski <machnikowski@gmail.com>
2025-03-23 15:23:41 +0100

    maintenance mode

--- src/config/site.ts
+++ src/config/site.ts
@@ -7,7 +7,7 @@ export const siteConfig = {
    * When true, shows the maintenance page to all visitors
    * When false, the main site is accessible to everyone
    */
-  maintenanceMode: false,
+  maintenanceMode: true,
   
   /**
    * Access key that bypasses maintenance mode when provided in the URL
