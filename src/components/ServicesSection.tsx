0852618b2f7f9dbd6ecb01cc3ce18ccbcaf76c36
Martin.Machnikowski <machnikowski@gmail.com>
2025-03-30 22:14:38 +0200

    adjusted service section colors

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -7,7 +7,7 @@ const ServicesSection = () => {
   const allProducts = products;
 
   return (
-    <section id="services" className="py-20 bg-muted">
+    <section id="services" className="py-20">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl md:text-4xl font-semibold text-center mb-12 fade-in text-primary font-bryndan uppercase">
           NASZA OFERTA
