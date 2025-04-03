ece5308d190b516f3806d429e320f0c720a70ae4
Martin.Machnikowski <machnikowski@gmail.com>
2025-04-03 14:53:07 +0200

    updated dog icons and cleaned up the products section

--- src/types/product.ts
+++ src/types/product.ts
@@ -2,17 +2,6 @@
 export interface Product {
   id: string;
   title: string;
-  description: string;
-  price: string;
   link: string;
-  showOnHomepage: boolean;
   homepagePriority: number;
-  isNew?: boolean;
-  discount?: {
-    percentage: number;
-    validUntil: Date;
-  };
-  features: string[];
-  duration: string;
-  groupSize: string;
 }
