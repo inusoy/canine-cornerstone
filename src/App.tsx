8dbf90c78c6d3d66fa89837ec0757f1deb938120
Martin.Machnikowski <machnikowski@gmail.com>
2025-04-12 13:41:11 +0200

    refactor: Remove unused components and dependencies for cleaner codebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-
 import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -29,7 +27,6 @@ const App = () => (
     <TooltipProvider>
       <HelmetProvider>
         <Toaster />
-        <Sonner />
         <BrowserRouter>
           <ScrollToTop />
           <Routes>
